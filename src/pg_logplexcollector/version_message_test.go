1df108deae27e25ebb753fc9607ee12c60abc9ef
Daniel Farina <daniel@fdr.io>
2012-12-30 16:39:41 -0800

    Version message tests
    Tests the positive case and two negative cases, where the message
    string is malformed or there is some error getting the message.
    Provides an OK example on how to use the passed closures to inject
    results and faults.
    
    Signed-off-by: Daniel Farina <daniel@fdr.io>

---
+++ src/pg_logplexcollector/version_message_test.go
@@ -0,0 +1,88 @@
+// Tests error handling and support of various version ('V') messages.
+//
+// Also serves as an example on how to use indirections provided by
+// pg_logplexcollector to do testing.
+package main
+
+import (
+	"bytes"
+	"errors"
+	"femebe"
+	"testing"
+)
+
+func TestVersionOK(t *testing.T) {
+	msgInit := func(dst *femebe.Message, exit exitFn) {
+		buf := bytes.Buffer{}
+		femebe.WriteCString(&buf, "PG9.2.2/1")
+
+		dst.InitFromBytes('V', buf.Bytes())
+	}
+
+	exit := func(args ...interface{}) {
+		t.Fatal("Ver Message is thought to be well formed. " +
+			"Invariant broken.")
+	}
+
+	processVerMsg(msgInit, exit)
+}
+
+func TestVersionBadValue(t *testing.T) {
+	msgInit := func(dst *femebe.Message, exit exitFn) {
+		buf := bytes.Buffer{}
+		femebe.WriteCString(&buf, "PG7.4.15/1")
+
+		dst.InitFromBytes('V', buf.Bytes())
+	}
+
+	sentinel := &msgInit
+
+	exit := func(args ...interface{}) {
+		// Because this kind of error simply reports a string (with no
+		// type taxonomy to go with it), and that seems good enough,
+		// elide confirming precisely what the error is.
+		panic(sentinel)
+	}
+
+	defer func() {
+		if r := recover(); r != nil && r != sentinel {
+			t.Fatal("Paniced, but not with the sentinel value")
+		}
+
+		// Success
+	}()
+
+	processVerMsg(msgInit, exit)
+	t.Fatal("Message should call exit, aborting execution before this")
+}
+
+func TestVersionMsgInitErr(t *testing.T) {
+	theErr := errors.New("An error; e.g. network difficulties")
+
+	msgInit := func(dst *femebe.Message, exit exitFn) {
+		exit(theErr)
+	}
+
+	sentinel := &msgInit
+
+	exit := func(args ...interface{}) {
+		// Since the error instance raised is injected, test that it
+		// is precisely the error propagated to the exitFn.
+		if args[0] != theErr {
+			t.Fatal("Error propagated, but not the expected one")
+		}
+
+		panic(sentinel)
+	}
+
+	defer func() {
+		if r := recover(); r != nil && r != sentinel {
+			t.Fatal("Paniced, but not with the sentinel value")
+		}
+
+		// Success
+	}()
+
+	processVerMsg(msgInit, exit)
+	t.Fatal("Message should call exit, aborting execution before this")
+}
